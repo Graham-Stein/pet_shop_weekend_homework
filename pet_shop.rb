6dc33bf7c285d938b81354712796c8ce7b1beec0
Graham-Stein <graham@guavoaviation.co.uk>
2018-09-14 16:27:25 +0100

    Add up to all_pets_by_breed__found

--- pet_shop.rb
+++ pet_shop.rb
@@ -9,3 +9,31 @@ end
 def add_or_remove_cash(shop, cash_value)
   shop[:admin][:total_cash] += cash_value
 end
+
+def pets_sold(shop)
+  shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, delta_pets)
+  shop[:admin][:pets_sold] += delta_pets
+end
+
+def stock_count(shop)
+  shop[:pets].length
+end
+
+def pets_by_breed(shop,pet)
+  animals = shop[:pets]
+
+  total = []
+  for animal in animals
+    puts "#{animal} IN LOOP"
+    puts animal[:breed]
+    puts pet
+    if animal[:breed] == pet
+      total.push(animal)
+    end
+    puts "#{total.count} IN ARRAY"
+  end
+  return total
+end
